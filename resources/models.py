38fb9002b9133717678d8c01265f34c6f72c9a3f
Joel Ogalla <jogalla@inspladelestany.cat>
2026-03-05 18:42:45 +0100

    taula recursos creada

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Recurs(models.Model):
+
+    class Categoria(models.TextChoices):
+        LLIBRE = 'LL', 'Llibre'
+        VIDEO = 'VI', 'Vídeo'
+        CURS = 'CU', 'Curs'
+
+    titol = models.CharField(max_length=200)
+    descripcio = models.TextField(blank=True)
+    categoria = models.CharField(
+        max_length=2,
+        choices=Categoria.choices,
+        default=Categoria.LLIBRE
+    )
+    data_publicacio = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titol
